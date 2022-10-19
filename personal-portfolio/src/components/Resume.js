8ebb40d5f7fe1538b4e5b881a6a6812ee99b5d3a
Justin <justinrkinsman@gmail.com>
2022-10-19 15:59:30 -0300

    Add CSS styling to resume section

--- personal-portfolio/src/components/Resume.js
+++ personal-portfolio/src/components/Resume.js
@@ -1,11 +1,14 @@
 import ResumeImage from '../images/resume.jpg'
+import './Resume.css'
 
 export const Resume = () => {
     return (
         <div className='resumeSection'>
             <h2>Resume</h2>
-            <img src={ResumeImage} alt='Resume Placeholder'></img>
-            <button>Download PDF</button>
+            <div className="resumeContent">
+                <img src={ResumeImage} alt='Resume Placeholder'></img>
+                <button>Download PDF</button>
+            </div>
         </div>
     )
 }
