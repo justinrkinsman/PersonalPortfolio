7fa6d419e8c0110aca51bf65ce2d08ffbfd0d7c2
Justin <justinrkinsman@gmail.com>
2022-10-19 15:44:23 -0300

    Add CSS styling to contact section

--- personal-portfolio/src/components/Contact.js
+++ personal-portfolio/src/components/Contact.js
@@ -1,12 +1,20 @@
+import './Contact.css'
+
 export const Contact = () => {
     return(
         <div className='contactSection'>
             <h2>Contact Information</h2>
-            <p>Justin Kinsman</p>
-            <p>Phone number</p>
-            <p>Email</p>
-            <p>Github</p>
-            <p>LinkedIn</p>
+            <h4>Justin Kinsman</h4>
+            <div className="contactInfo">
+                <div className="phoneAndEmail">
+                    <p>Phone: (902) 979-3227</p>
+                    <p>Email: justinrkinsman@gmail.com</p>
+                </div>
+                <div className="links">
+                    <p>Github</p>
+                    <p>LinkedIn</p>
+                </div>
+            </div>
         </div>
     )
 }
