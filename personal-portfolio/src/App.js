53837190de4931a0ae10dbebfb029e192c424369
Justin <justinrkinsman@gmail.com>
2022-10-19 11:56:10 -0300

    Add contact information section

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -5,6 +5,7 @@ import About from './components/About';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Resume from './components/Resume';
+import { Contact } from './components/Contact';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Experience />
       <Projects />
       <Resume />
+      <Contact />
     </div>
   );
 }
---
+++ personal-portfolio/src/components/Contact.js
@@ -0,0 +1,12 @@
+export const Contact = () => {
+    return(
+        <div className='contactSection'>
+            <h2>Contact Information</h2>
+            <p>Justin Kinsman</p>
+            <p>Phone number</p>
+            <p>Email</p>
+            <p>Github</p>
+            <p>LinkedIn</p>
+        </div>
+    )
+}
