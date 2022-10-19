d78c4ad9e16d131e54801b3fa459270900ee8e9f
Justin <justinrkinsman@gmail.com>
2022-10-19 11:52:07 -0300

    Add section for projects

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -3,6 +3,7 @@ import { Navbar } from './components/Header';
 import LandingPage from './components/Home';
 import About from './components/About';
 import Experience from './components/Experience';
+import Projects from './components/Projects';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <LandingPage />
       <About />
       <Experience />
+      <Projects />
     </div>
   );
 }
---
+++ personal-portfolio/src/components/Projects.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Projects extends React.Component {
+    render() {
+        return (
+            <div className='projectsSection'>
+                <h2>Projects</h2>
+                <p>Grid containing the individual project sections</p>
+            </div>
+        )
+    }
+}
+/*Make a subcomponent for individual project cards*/
+export default Projects
