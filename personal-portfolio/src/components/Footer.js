d763d79964e8e0e913edc732b43a06fdaa518e73
Justin <justinrkinsman@gmail.com>
2022-10-19 15:49:07 -0300

    Add CSS styling to footer

--- personal-portfolio/src/components/Footer.js
+++ personal-portfolio/src/components/Footer.js
@@ -1,10 +1,14 @@
+import './Footer.css'
+
 export const Footer = () => {
     return(
         <div className='footer'>
             <p>Developed by Justin Kinsman</p>
             <p>Copyright c 2022</p>
-            <p>Github</p>
-            <p>LinkedIn</p>
+            <div className='footerLinks'>
+                <p>Github</p>
+                <p>LinkedIn</p>
+            </div>
         </div>
     )
 }
