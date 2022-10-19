021f983b3d4e3eda6d5a008bef8c0e95c73e2c35
Justin <justinrkinsman@gmail.com>
2022-10-19 11:49:43 -0300

    Add experience section

---
+++ personal-portfolio/src/components/Experience.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class Experience extends React.Component {
+    render() {
+        return (
+            <div className='experienceSection'>
+                <div className='handsOnExperience'>
+                    <h2>Experience</h2>
+                    <p>The Odin Project</p>
+                </div>
+                <div className="tools">
+                    <h2>Languages and Tools I Use</h2>
+                    <p>grid showing icons of all the languages and tools I've used</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Experience
