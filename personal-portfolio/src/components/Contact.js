ea96e5ace824dae5b8a9c48b3b06084c0dd28f36
Justin <justinrkinsman@gmail.com>
2022-10-20 08:25:12 -0300

    Add additional text to contact section

--- personal-portfolio/src/components/Contact.js
+++ personal-portfolio/src/components/Contact.js
@@ -5,6 +5,9 @@ export const Contact = () => {
         <div className='contactSection'>
             <h2>Contact Information</h2>
             <h4>Justin Kinsman</h4>
+            <p>If any of what you've seen here interests you or if you have questions about any of my projects
+                please feel free to contact me.
+            </p>
             <div className="contactInfo">
                 <div className="phoneAndEmail">
                     <p>Phone: (902) 979-3227</p>
