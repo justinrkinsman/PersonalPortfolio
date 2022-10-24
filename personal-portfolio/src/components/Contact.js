926c5f702aa40ca8d0bdbffa35a97ef550b6692e
Justin <justinrkinsman@gmail.com>
2022-10-24 15:22:36 -0300

    adjust layout of contact section

--- personal-portfolio/src/components/Contact.js
+++ personal-portfolio/src/components/Contact.js
@@ -7,15 +7,12 @@ export const Contact = () => {
             <div className='contactSection' id='contactPage'>
                 <h2 id='resumeHeader' className='header'>Contact Information</h2>
                 <h4 className='secondaryHeader'>Justin Kinsman</h4>
-                <p>If any of what you've seen here interests you or if you have questions about any of my projects
-                    please feel free to contact me.
+                <p>If anything you've seen here is of interest and you would like to discuss possible 
+                    employment or if you have any questions please feel free to contact me.
                 </p>
                 <div className="contactInfo">
-                    <div className="phoneAndEmail">
-                        <p>Phone: (902) 979-3227</p>
-                        <p>Email: justinrkinsman@gmail.com</p>
-                    </div>
                     <div className="links">
+                        <p>Email</p>
                         <p>Github</p>
                         <p>LinkedIn</p>
                     </div>
