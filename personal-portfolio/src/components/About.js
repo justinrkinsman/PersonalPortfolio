9c9be8c198a78344ae74b2119f3e27c2c6eb4fab
Justin <justinrkinsman@gmail.com>
2022-10-24 12:53:57 -0300

    Change layout of about section

--- personal-portfolio/src/components/About.js
+++ personal-portfolio/src/components/About.js
@@ -34,23 +34,19 @@ export function About() {
         }
     }, [])*/
     return (
-        <>
-            <div id='aboutPageStickyHeader' className='pageStickyHeader'></div>
-            <div className='aboutSection' id='aboutPage'>
-                <h2 id='aboutHeader' className='header'>About</h2>
+        <div className='aboutSection' id='aboutPage'>
+            <h2 id='aboutHeader' className='header'>About</h2>
+            <h3 className='bioHeader secondaryHeader'>Bio</h3>
+            <div className="aboutSectionContent">
                 <div className="bio">
-                    <h3 className='bioHeader secondaryHeader'>Bio</h3>
-                    <p>In 2022, after many false starts and dead-end jobs, I decided to finally get off my 
-                        butt and pursue my goal of becoming a software engineer. I landed on JavaScript as
-                        the language I wanted to learn and started down the path of web development. I 
-                        treated learning to code as a full-time job and began coding projects for 6-8 hours 
-                        every weekday. This past year has been an eye-opening experience and I can truly say,
-                        with all sincerity, that learning to code has changed my life. I am so excited to
-                        bring my new-found skills to the workforce and start my new life as a software
-                        engineer.
+                    <p className='bioText'>In 2022, after many false starts and dead-end jobs, I decided to finally get off my butt
+                        and pursue my goal of becoming a software engineer. I landed on JavaScript as the
+                        language I wanted to learn and started down the path of web development. I treated
+                        learning to code as a full-time job and began coding projects for 6-8 hours every
+                        weekday. This past year has been an eye-opening experience and I can truly say, with
+                        all sincerity, that learning to code has changed my life. I am so excited to bring my
+                        new-found skills to the workforce and start my new life as a software engineer.
                     </p>
-                </div>
-                <div className='aboutMe'>
                     <div className="aboutMeText">
                         <h3>About Me</h3>
                         <p>Outside of coding my hobbies include:</p>
@@ -61,9 +57,16 @@ export function About() {
                             <li>Collecting obscure movies</li>
                         </ul>
                     </div>
-                    <img src={Working} alt='Woman working at desk'></img>
                 </div>
+                <img src={Working} alt='Woman working at desk'></img>
             </div>
-        </>
+        </div>
     )
 }
+
+/*
+In 2022, after one too many failed job interviews, I had finally had enough. I was fed up with not having any 
+skills to put on a resume. I decided that it was time to finally get off my butt and start pursuing my goal
+of becoming a software engineer. But not only was I going to learn to code, I was going to get so good that 
+employers would have no choide but to hire me.
+*/
