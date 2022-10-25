5424ad816deb84064922d0e14ea69cd7bb882ffa
Justin <justinrkinsman@gmail.com>
2022-10-25 14:14:19 -0300

    Adjust layout of contact section

--- personal-portfolio/src/components/Contact.js
+++ personal-portfolio/src/components/Contact.js
@@ -2,22 +2,28 @@ import './Contact.css'
 
 export const Contact = () => {
     return(
-        <>
-            <div id='contactPageStickyHeader' className='pageStickyHeader'></div>
-            <div className='contactSection' id='contactPage'>
-                <h2 id='resumeHeader' className='header'>Contact Information</h2>
-                <h4 className='secondaryHeader'>Justin Kinsman</h4>
-                <p>If anything you've seen here is of interest and you would like to discuss possible 
-                    employment or if you have any questions please feel free to contact me.
-                </p>
-                <div className="contactInfo">
-                    <div className="links">
+        <div className='contactSection' id='contactPage'>
+            <h2 id='resumeHeader' className='header'>Contact Information</h2>
+            <h4>Justin Kinsman</h4>
+            <p>If anything you've seen here is of interest and you would like to discuss possible 
+                employment or if you have any questions please feel free to contact me.
+            </p>
+            <div className="contactInfo">
+                <div className="links">
+                    <div className="emailLink">
                         <p>Email</p>
+                        <p>justinrkinsman@gmail.com</p>
+                    </div>
+                    <div className="githubLink">
                         <p>Github</p>
+                        <p>https://github.com/justinrkinsman</p>
+                    </div>
+                    <div className="linkedInLink">
                         <p>LinkedIn</p>
+                        <p>https://www.linkedin.com/in/justin-kinsman</p>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
