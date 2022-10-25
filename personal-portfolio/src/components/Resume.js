fe32822fa52a8647e605abc6b7c2b9c7f96ff087
Justin <justinrkinsman@gmail.com>
2022-10-25 13:40:48 -0300

    Add resume screenshot to resume section

--- personal-portfolio/src/components/Resume.js
+++ personal-portfolio/src/components/Resume.js
@@ -1,17 +1,15 @@
 import ResumeImage from '../images/resume.jpg'
 import './Resume.css'
+import ResumePDF from '../images/Resume-PDF.pdf'
 
 export const Resume = () => {
     return (
-        <>
-            <div id='resumePageStickyHeader' className='pageStickyHeader'></div>
-            <div className='resumeSection' id='resumePage'>
-                <h2 id='resumeHeader' className='header'>Resume</h2>
-                <div className="resumeContent secondaryHeader">
-                    <img src={ResumeImage} alt='Resume Placeholder'></img>
-                    <button>Download PDF</button>
-                </div>
+        <div className='resumeSection' id='resumePage'>
+            <h2 id='resumeHeader' className='header'>Resume</h2>
+            <div className="resumeContent">
+                <iframe title='ResumePDFViewer' className='resumeScreenshot' src={ResumePDF}></iframe>
+                <button>Download PDF</button>
             </div>
-        </>
+        </div>
     )
 }
