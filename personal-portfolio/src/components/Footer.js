d8c94941c023f1d8e7a713685e01a9af49b0f40f
Justin <justinrkinsman@gmail.com>
2022-10-26 10:28:23 -0300

    Add copyright symbol to footer

--- personal-portfolio/src/components/Footer.js
+++ personal-portfolio/src/components/Footer.js
@@ -4,7 +4,7 @@ export const Footer = () => {
     return(
         <div className='footer'>
             <p>Developed by Justin Kinsman</p>
-            <p>Copyright c 2022</p>
+            <p>Copyright © 2022</p>
             <div className='footerLinks'>
                 <p>Github</p>
                 <p>LinkedIn</p>
