36546eee655493f8fc873ce29bcb91e823030602
Justin <justinrkinsman@gmail.com>
2022-10-28 10:26:55 -0300

    Add mobile view to header

--- personal-portfolio/src/components/Header.js
+++ personal-portfolio/src/components/Header.js
@@ -5,12 +5,12 @@ export const Navbar = () => {
     return(
         <>
             <div className='navbar sticky'>
-                <a className='activeNavLink' href='#homePage'>Home</a>
-                <a href='#aboutPage'>About</a>
-                <a href='#experiencePage'>Experience</a>
-                <a href='#projectsPage'>Projects</a>
-                <a href='#resumePage'>Resume</a>
-                <a href='#contactPage'>Contact</a>
+                <a className='navLink' href='#homePage'>Home</a>
+                <a className='navLink' href='#aboutPage'>About</a>
+                <a className='navLink' href='#experiencePage'>Experience</a>
+                <a className='navLink' href='#projectsPage'>Projects</a>
+                <a className='navLink' href='#resumePage'>Resume</a>
+                <a className='navLink' href='#contactPage'>Contact</a>
             </div>
         </>
     )
