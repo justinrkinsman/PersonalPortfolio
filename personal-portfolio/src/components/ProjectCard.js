40056c33460159b1c9975413436e96c7cf953de6
Justin <justinrkinsman@gmail.com>
2022-10-31 09:36:38 -0300

    Add hover text to repo and preview links

--- personal-portfolio/src/components/ProjectCard.js
+++ personal-portfolio/src/components/ProjectCard.js
@@ -9,8 +9,8 @@ export const ProjectCard = (props) => {
             <div className="imageAndLinks">
                 <img className='projectScreenshot' src={props.source} alt={props.alt}></img>
                 <div className='projectLinks'>
-                    <a href={props.repo}><img src={GitHubIcon} alt='Link to GitHub Repository' className='repositoryLink'></img></a>
-                    <a href={props.preview}><img src={LinkIcon} alt='Link to live preview of project' className='previewLink'></img></a>
+                    <a title="Repository" href={props.repo}><img src={GitHubIcon} alt='Link to GitHub Repository' className='repositoryLink'></img></a>
+                    <a title="Live Preview" href={props.preview}><img src={LinkIcon} alt='Link to live preview of project' className='previewLink'></img></a>
                 </div>
             </div>
             <div className="projectCardTextContent">
