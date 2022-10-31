8f94e2ffe76c60287eed9caea1d3cae907ebe90b
Justin <justinrkinsman@gmail.com>
2022-10-31 09:54:41 -0300

    Make repo and preview links open a new tab

--- personal-portfolio/src/components/ProjectCard.js
+++ personal-portfolio/src/components/ProjectCard.js
@@ -9,8 +9,8 @@ export const ProjectCard = (props) => {
             <div className="imageAndLinks">
                 <img className='projectScreenshot' src={props.source} alt={props.alt}></img>
                 <div className='projectLinks'>
-                    <a title="Repository" href={props.repo}><img src={GitHubIcon} alt='Link to GitHub Repository' className='repositoryLink'></img></a>
-                    <a title="Live Preview" href={props.preview}><img src={LinkIcon} alt='Link to live preview of project' className='previewLink'></img></a>
+                    <a title="Repository" target='_blank' rel='noopener noreferrer' href={props.repo}><img src={GitHubIcon} alt='Link to GitHub Repository' className='repositoryLink'></img></a>
+                    <a title="Live Preview" target='_blank' rel='noopener noreferrer' href={props.preview}><img src={LinkIcon} alt='Link to live preview of project' className='previewLink'></img></a>
                 </div>
             </div>
             <div className="projectCardTextContent">
