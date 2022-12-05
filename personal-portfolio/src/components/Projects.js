81304ed761074361024cfb9fb243de37fccbeb51
Justin <justinrkinsman@gmail.com>
2022-12-05 14:16:01 -0400

    Add members only project to projects page

--- personal-portfolio/src/components/Projects.js
+++ personal-portfolio/src/components/Projects.js
@@ -2,13 +2,15 @@ import React, { useState } from 'react'
 import { ProjectCard } from './ProjectCard'
 import Memory from '../images/memory.jpg'
 import Library from '../images/library.jpg'
+import Members_Only from '../images/members-only.jpg'
 import './Projects.css'
 
 function Projects() {
     
     const [images] = useState([
         ['Library Project Screenshot', Library],
-        ['Memory Card Project Screenshot', Memory]
+        ['Memory Card Project Screenshot', Memory],
+        ['Members Only Project Screenshot', Members_Only]
     ])
     
     return (    
@@ -16,7 +18,7 @@ function Projects() {
             <h2 id='projectsHeader' className='header'>Projects</h2>
             <div className="projectGrid secondaryHeader projectTest">
                 <ProjectCard className='project-screenshot' source={images[0][1]} alt={images[0][0]} title={LIBRARY.title} languages={LIBRARY.languages} description={LIBRARY.description} preview={LIBRARY.preview} repo={LIBRARY.repo} />
-                <ProjectCard className='project-screenshot' source={images[0][1]} alt={images[0][0]} title={BATTLESHIP.title} languages={BATTLESHIP.languages} description={BATTLESHIP.description} preview={LIBRARY.preview} repo={LIBRARY.repo}/>
+                <ProjectCard className='project-screenshot' source={images[2][1]} alt={images[2][0]} title={MEMBERS_ONLY.title} languages={MEMBERS_ONLY.languages} description={MEMBERS_ONLY.description} preview={MEMBERS_ONLY.preview} repo={MEMBERS_ONLY.repo}/>
                 <ProjectCard className='project-screenshot' source={images[1][1]} alt={images[1][0]} title={MEMORY.title} languages={MEMORY.languages} description={MEMORY.description} preview={MEMORY.preview} repo={MEMORY.repo}/>
             </div>
         </div>
@@ -31,12 +33,12 @@ const LIBRARY = {
     repo: 'https://github.com/justinrkinsman/library-livepreview'
 }
 
-const BATTLESHIP = {
-    title: 'Battleship',
-    languages: 'HTML, CSS, JavaScript, Jest',
-    description: 'Battleship game based on the popular board game',
-    preview: 'https://justinrkinsman.github.io/battleship/',
-    repo: 'https://github.com/justinrkinsman/library-livepreview'
+const MEMBERS_ONLY = {
+    title: 'Members Only',
+    languages: 'ejs, pug, mongoose, mongoDB, express, JavaScript',
+    description: 'Chatroom where users can create an account to create, read, update, and delete posts. Features password hashing and salting',
+    preview: 'https://membersonly-production.up.railway.app/',
+    repo: 'https://github.com/justinrkinsman/MembersOnly'
 }
 
 const MEMORY = {
