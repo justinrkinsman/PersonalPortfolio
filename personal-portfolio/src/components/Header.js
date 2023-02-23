352a8a02cbfe76145a2fba4a9c3728d94af0901c
Justin <justinrkinsman@gmail.com>
2023-02-23 14:05:09 -0400

    Add smooth scrolling to navbar

--- personal-portfolio/src/components/Header.js
+++ personal-portfolio/src/components/Header.js
@@ -2,15 +2,27 @@ import './Header.css'
 //import { HashLink as Link } from 'react-router-hash-link'
 
 export const Navbar = () => {
+    const load = (targetId, e) => {
+        e.preventDefault();
+        const target = document.getElementById(targetId);
+        target.scrollIntoView({ behavior: 'smooth' });
+        window.history.pushState({}, '', '');
+    }
+
+    function loadHeader(e) {
+        e.preventDefault();
+        document.documentElement.scrollIntoView({ behavior: 'smooth' });
+        window.history.pushState({}, '', '');
+    }
     return(
         <>
             <div className='navbar sticky'>
-                <a className='navLink' href='#homePage'>Home</a>
-                <a className='navLink' href='#aboutPage'>About</a>
-                <a className='navLink' href='#experiencePage'>Experience</a>
-                <a className='navLink' href='#projectsPage'>Projects</a>
-                <a className='navLink' href='#resumePage'>Resume</a>
-                <a className='navLink' href='#contactPage'>Contact</a>
+                <a className='navLink' href='#homePage' onClick={loadHeader}>Home</a>
+                <a className='navLink' href='#aboutPage' onClick={(e) => load('aboutPage', e)}>About</a>
+                <a className='navLink' href='#experiencePage' onClick={(e) => load('experiencePage', e)}>Experience</a>
+                <a className='navLink' href='#projectsPage' onClick={(e) => load('projectsPage', e)}>Projects</a>
+                <a className='navLink' href='#resumePage' onClick={(e) => load('resumePage', e)}>Resume</a>
+                <a className='navLink' href='#contactPage' onClick={(e) => load('contactPage', e)}>Contact</a>
             </div>
         </>
     )
