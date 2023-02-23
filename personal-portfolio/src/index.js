5acb4e86f17622367d1ae0bdc83be8d9a664d7b1
Justin <justinrkinsman@gmail.com>
2023-02-23 14:57:23 -0400

    Remove unused link at the bottom of the page

--- personal-portfolio/src/index.js
+++ personal-portfolio/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ProjectsPage } from './components/AllProjects';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,7 +13,6 @@ root.render(
           <Route path='/all-projects' element={<ProjectsPage />} />
           <Route path='*' element={<App />} />
       </Routes>
-      <Link to='/all-projects'>All Projects</Link>
     </BrowserRouter>
   </React.StrictMode>
 );
