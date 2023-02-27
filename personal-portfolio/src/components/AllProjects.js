bc898b10b7aecaef2b6ee8e3e739a674cb03aadc
Justin <justinrkinsman@gmail.com>
2023-02-27 10:51:01 -0400

    Correct punctuation in project descriptions

--- personal-portfolio/src/components/AllProjects.js
+++ personal-portfolio/src/components/AllProjects.js
@@ -43,7 +43,7 @@ export function ProjectsPage() {
 const LIBRARY = {
     title: 'Library',
     languages: 'HTML, CSS, and JavaScript',
-    description: 'Library app to keep track of books read',
+    description: 'Library app to keep track of books read.',
     preview: 'https://justinrkinsman.github.io/library-livepreview/',
     repo: 'https://github.com/justinrkinsman/library-livepreview'
 }
@@ -51,7 +51,7 @@ const LIBRARY = {
 const MEMBERS_ONLY = {
     title: 'Members Only',
     languages: 'ejs, pug, mongoose, mongoDB, express, JavaScript',
-    description: 'Chatroom where users can create an account to create, read, update, and delete posts. Features password hashing and salting',
+    description: 'Chatroom where users can create an account to create, read, update, and delete posts. Features password hashing and salting.',
     preview: 'https://membersonly-production.up.railway.app/',
     repo: 'https://github.com/justinrkinsman/MembersOnly'
 }
@@ -59,7 +59,7 @@ const MEMBERS_ONLY = {
 const MEMORY = {
     title: 'Memory Card',
     languages: 'HTML, CSS, JavaScript, ReactJS',
-    description: 'The object of this game is to not click the same card twice',
+    description: 'The object of this game is to not click the same card twice.',
     preview: 'https://justinrkinsman.github.io/MemoryCard-LivePreview/',
     repo: 'https://github.com/justinrkinsman/MemoryCard-LivePreview'
 }
@@ -67,7 +67,7 @@ const MEMORY = {
 const BLOG_API = {
     title: 'Blog API',
     languages: 'JavaScript, Pug, CSS, EJS',
-    description: 'Blog site using REST API for the backend to host two separate front-ends: one for blog owner to make posts, one for users to sign in and leave comments',
+    description: 'Blog site using REST API for the backend to host two separate front-ends: one for blog owner to make posts, one for users to sign in and leave comments.',
     preview: 'https://github.com/justinrkinsman/Blog_API',
     repo: 'https://github.com/justinrkinsman/Blog_API'
 }
@@ -75,7 +75,7 @@ const BLOG_API = {
 const INSTABOOK = {
     title: "InstaBook",
     languages: 'JavaScript, Pug, CSS, EJS, Mongoose, Express',
-    description: 'Social media site that allows users to view posts and add friends. Users can only see posts of people they are friends with',
+    description: 'Social media site that allows users to view posts and add friends. Users can only see posts of people they are friends with.',
     preview: 'https://github.com/justinrkinsman/InstaBook',
     repo: 'https://github.com/justinrkinsman/InstaBook'
 }
