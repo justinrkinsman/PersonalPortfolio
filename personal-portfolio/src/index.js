7d4f13f0fbb2cba4a5d8784fb3b92ce767acb2f7
Justin <justinrkinsman@gmail.com>
2023-03-06 10:52:37 -0400

    Page scrolls to top on rendering new page

--- personal-portfolio/src/index.js
+++ personal-portfolio/src/index.js
@@ -4,11 +4,24 @@ import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ProjectsPage } from './components/AllProjects';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
           <Route path='/all-projects' element={<ProjectsPage />} />
           <Route path='*' element={<App />} />
